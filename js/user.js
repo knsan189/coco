78bf48135ae150e8f8083f0d35932c2ce089b85d
Haneul Jin <77265562+knsan189@users.noreply.github.com>
2021-02-05 20:09:42 +0900

    Add files via upload
    제이쿼리에서 바닐라로 변경

--- js/user.js
+++ js/user.js
@@ -18,27 +18,26 @@ $(document).ready(function () {
     });
 });
 
-$(function(){
+$(function () {
     var num = $(this).index();
-    $('.cs_board .tabmenu > li').on('click', function(){
+    $('.cs_board .tabmenu > li').on('click', function () {
         $(this).addClass('active')
-        .siblings().removeClass('active')
+            .siblings().removeClass('active')
         $(this).parent().next().children()
-        .eq(num).addClass('active')
-        .siblings().removeClass('active')
+            .eq(num).addClass('active')
+            .siblings().removeClass('active')
     })
 })
 
-$(function(){
-    $('.article1 .plpa').on('click', function(){
-        var iButton = $(this).children() 
+$(function () {
+    $('.article1 .plpa').on('click', function () {
+        var iButton = $(this).children()
 
-        if(iButton.hasClass('fa-pause')){
+        if (iButton.hasClass('fa-pause')) {
             $('.slide_group').slick('slickPause')
             iButton.removeClass('fa-pause')
             iButton.addClass('fa-play')
-        }
-        else{
+        } else {
             $('.slide_group').slick('slickPlay')
             iButton.removeClass('fa-play')
             iButton.addClass('fa-pause')
@@ -46,34 +45,95 @@ $(function(){
         }
     })
 })
-$(function(){
-    var ww = $(window).width()
-    var depth1Li = $('.depth1 > li')
-    if(ww > 1024){
-        depth1Li.hover(
-            function(){
-                $(this).children('.depth2').addClass('on')
-            },
-            function(){
-                $(this).children('.depth2').removeClass('on')
-            })
-    }
-    else{
-    var mButton = $('#header button > i')
-    mButton.click(function(){
-        depth1Li.toggleClass('on')
-        if($('.depth1 > li').hasClass('on')){
-            $(this).addClass('fa-times').removeClass('fa-bars')
-        }
-        else{
-            $(this).addClass('fa-bars').removeClass('fa-times')
-        }
-    })
-    depth1Li.click(function(){
-        $(this).children('.depth2').toggleClass('on')
-        $(this).siblings().children('.depth2').removeClass('on')
-    })
-    
+// $(function(){
+//     var ww = $(window).width()
+//     var depth1Li = $('.depth1 > li')
+//     if(ww > 1024){
+//         depth1Li.hover(
+//             function(){
+//                 $(this).children('.depth2').addClass('on')
+//             },
+//             function(){
+//                 $(this).children('.depth2').removeClass('on')
+//             })
+//     }
+//     else{
+//     var mButton = $('#header button > i')
+//     mButton.click(function(){
+//         depth1Li.toggleClass('on')
+//         if($('.depth1 > li').hasClass('on')){
+//             $(this).addClass('fa-times').removeClass('fa-bars')
+//         }
+//         else{
+//             $(this).addClass('fa-bars').removeClass('fa-times')
+//         }
+//     })
+//     depth1Li.click(function(){
+//         $(this).children('.depth2').toggleClass('on')
+//         $(this).siblings().children('.depth2').removeClass('on')
+//     })
+//     }
+// })
 
+// window.onload = function(){
+//     var depth1Li = document.querySelector('.depth1 > li')
+//     var nextSiblings = depth1Li.nextElementSibling;
+
+//     while(nextSiblings){
+//         console.log(nextSiblings);
+//         nextSiblings = nextSiblings.nextElementSibling;
+//     }
+
+
+// }
+window.onload = function Navgaitor() {
+
+    var sw = document.body.offsetWidth; // 화면 사이즈
+    var mBtn = document.querySelector('#header button > i') // 메뉴 버튼
+    var nav = document.getElementById('nav') // 네비게이션
+    var depth1Li = document.querySelectorAll('.depth1 > li')
+
+    for (var i = 0; i < depth1Li.length; i++) {
+        //PC 환경
+        if (sw > 1024) {
+            depth1Li[i].addEventListener('mouseover', function () {
+                this.children[1].classList.add('on')
+            })
+            depth1Li[i].addEventListener('mouseout', function () {
+                this.children[1].classList.remove('on')
+            });
+
+        }
+
+        //모바일 환경
+        else{
+            mBtn.addEventListener('click', function(){
+                if(!(nav.classList.contains('on'))){
+                    nav.classList.add('on')
+                    this.classList.add('fa-times')
+                    this.classList.remove('fa-bars')
+                }
+                else{
+                    nav.classList.remove('on')
+                    this.classList.add('fa-bars')
+                    this.classList.remove('fa-times')
+                }                
+            })
+            depth1Li[i].addEventListener('click', function(){
+                if(!this.children[1].classList.contains('on')){
+                    this.children[1].classList.add('on')
+                }
+                else{
+                    this.children[1].classList.remove('on')
+                }
+                for(var siblings of this.parentNode.children){
+                    if(siblings !== this){
+                        siblings.children[1].classList.remove('on')
+                    }
+                }
+            })
+
+        }
+        
     }
-})
+}
